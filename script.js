dbae9a86c9cf5751568121a1797feb7b1ddc9820
AODBloodOath <themoonisout91@gmail.com>
2025-02-25 02:27:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('acceptPolicy').addEventListener('click', function() {
+    document.getElementById('monetizationContent').style.display = 'block';
+    this.style.display = 'none';
+});
